08ad2c3fd8a4d8c4b66c8f567d878fcaf16ef5a8
unknown <lukastyon@gmail.com>
2024-04-19 20:31:16 -0500

    Add imports from data file.

---
+++ main.js
@@ -0,0 +1,1 @@
+import { CHAMPIONS, BELTS } from "./src/data";
---
+++ src/data.js
@@ -0,0 +1,42 @@
+/**
+ * 1. TODO: Filter out the `Vacant Title` from the `CHAMPIONS` array. Also, remove the corresponding index from the `BELTS` array.
+ * 2. TODO: Filter out the `Tag Team Champions` from the `BELTS` array. Also, remove the corresponding indices from the `CHAMPIONS` array.
+ * 3. TODO: Alphabetize `CHAMPIONS` by last name.
+ * 4. TODO: Create a new ARRAY of objects called `CHAMPIONSHIPS` that contains the following properties:
+ *   - `title` - The name of the title.
+ *  - `champion` - The name of the champion.
+ */
+
+const CHAMPIONS = [
+  "Cody Rhodes",
+  "Damian Priest",
+  "Vacant Title",
+  "Bayley",
+  "Sami Zayn",
+  "Logan Paul",
+  "Awesome Truth",
+  "A-Town Down Under",
+  "The Kabuki Warriors",
+  "Ilja Dragunov ",
+  "Roxanne Perez",
+  "Oba Femi",
+  "Nathan Frazer & Axiom",
+  "Charlie Dempsey",
+];
+
+const BELTS = [
+  "Undisputed WWE Champion",
+  "World Heavyweight Champion",
+  "Women's World Champion",
+  "WWE Women’s Champion",
+  "Intercontinental Champion",
+  "United States Champion",
+  "World Tag Team Champions",
+  "SmackDown Tag Team Champions",
+  "WWE Women's Tag Team Champions",
+  "NXT Champion",
+  "NXT Women's Champion",
+  "NXT North American Champion",
+  "NXT Tag Team Champions",
+  "NXT Heritage Cup Champion",
+];
