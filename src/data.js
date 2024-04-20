be274326f90f355eead91f994245917d13144cdc
unknown <lukastyon@gmail.com>
2024-04-19 20:58:47 -0500

    filter out tag team champions from belts and champions arrays

--- src/data.js
+++ src/data.js
@@ -1,5 +1,4 @@
 /**
- * 1. TODO: Filter out the `Vacant Title` from the `CHAMPIONS` array. Also, remove the corresponding index from the `BELTS` array.
  * 2. TODO: Filter out the `Tag Team Champions` from the `BELTS` array. Also, remove the corresponding indices from the `CHAMPIONS` array.
  * 3. TODO: Alphabetize `CHAMPIONS` by last name.
  * 4. TODO: Create a new ARRAY of objects called `CHAMPIONSHIPS` that contains the following properties:
@@ -15,11 +14,9 @@ export const CHAMPIONS = [
   "Logan Paul",
   "Awesome Truth",
   "A-Town Down Under",
-  "The Kabuki Warriors",
   "Ilja Dragunov ",
   "Roxanne Perez",
   "Oba Femi",
-  "Nathan Frazer & Axiom",
   "Charlie Dempsey",
 ];
 
@@ -29,12 +26,8 @@ export const BELTS = [
   "WWE Women’s Champion",
   "Intercontinental Champion",
   "United States Champion",
-  "World Tag Team Champions",
-  "SmackDown Tag Team Champions",
-  "WWE Women's Tag Team Champions",
   "NXT Champion",
   "NXT Women's Champion",
   "NXT North American Champion",
-  "NXT Tag Team Champions",
   "NXT Heritage Cup Champion",
 ];
