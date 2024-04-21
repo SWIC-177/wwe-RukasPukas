e00640404f839044a734b2f59e412694d82a77b2
unknown <lukastyon@gmail.com>
2024-04-21 17:49:38 -0500

    reset main.js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
 import { BELTS, CHAMPIONS } from "./src/data";
-
-const filteredChampions = CHAMPIONS.filter(
-  (champion) => champion !== "Vacant Title",
-);
-
-const filteredBelts = BELTS.filter((belt) => belt !== "Women's World Champion");
