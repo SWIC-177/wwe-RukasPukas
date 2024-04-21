0607a2d8e740313fa00d2e0f4082c8e7525e262e
unknown <lukastyon@gmail.com>
2024-04-21 17:38:24 -0500

    create function to remove vacant title, create function to remove corresponding belt

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import { BELTS, CHAMPIONS } from "./src/data";
+
+const filteredChampions = CHAMPIONS.filter(
+  (champion) => champion !== "Vacant Title",
+);
+
+const filteredBelts = BELTS.filter((belt) => belt !== "Women's World Champion");
+
+console.log(filteredChampions);
+console.log(filteredBelts);
