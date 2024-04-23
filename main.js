f34c32cb16e2299e63c7935949e7741c28e5936f
unknown <lukastyon@gmail.com>
2024-04-22 22:59:08 -0500

    add updated arrays and test console outputs

--- main.js
+++ main.js
@@ -15,3 +15,7 @@ const { terms1: filteredBelts, terms2: filteredChampions } =
     terms2: CHAMPIONS,
     filterTerm: "Tag Team Champions",
   });
+export const UPDATED_BELTS = filteredBelts;
+export const UPDATED_CHAMPIONS = filteredChampions;
+console.log(UPDATED_BELTS);
+console.log(UPDATED_CHAMPIONS);
