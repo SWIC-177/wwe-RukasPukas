4240de4838956b067b0777cb7afdc49429399ae3
unknown <lukastyon@gmail.com>
2024-04-26 21:34:36 -0500

    remove todo 1 and 2

--- src/data.js
+++ src/data.js
@@ -1,7 +1,5 @@
 /**
- * 1. TODO: Remove `"Vacant Title" and the corresponding `"Women's World Champion"`.
- * 2. TODO: Remove all of 'Tag Team Champions' from the `BELTS` array and,
- *    simultaneously, remove all of the names in the `CHAMPIONS` array that correspond with those related indices.
+ 
  * 3. TODO: Alphabetize `CHAMPIONS` by last name.
  * 4. TODO: Create a new ARRAY of objects called `CHAMPIONSHIPS` that contains the following properties:
  *   - `title` - The name of the title.
