6cc566060ffa796f62d9e757bfe9a501ca418d50
unknown <lukastyon@gmail.com>
2024-05-01 22:11:05 -0500

    create initial failing test

--- src/lib.test.js
+++ src/lib.test.js
@@ -1,5 +1,9 @@
 import { expect, test, describe } from "vitest";
-import { removeCorrespondingItemsByTerm, alphabetizeNames } from "./lib";
+import {
+  removeCorrespondingItemsByTerm,
+  alphabetizeNames,
+  merge2ArraysIntoAnArrayOfObjects,
+} from "./lib";
 
 test("removeCorrespondingItemsByTerm", () => {
   // Arrange
@@ -84,4 +88,24 @@ describe("getLastName", () => {
     // Assert
     expect(result).toBe(expected);
   });
+});
+
+test("merge2ArraysIntoAnArrayOfObjects", () => {
+  // Arrange
+  const a1 = ["title1", "title2", "title3"];
+  const a2 = ["champion1", "champion2", "champion3"];
+  const key1 = "title";
+  const key2 = "champion";
+
+  const expected = [
+    { title: "title1", champion: "champion1" },
+    { title: "title2", champion: "champion2" },
+    { title: "title3", champion: "champion3" },
+  ];
+
+  // Act
+  const result = merge2ArraysIntoAnArrayOfObjects({ a1, a2, key1, key2 });
+
+  // Assert - Check if the function correctly merges the two arrays expect
+  expect(result).toEqual(expected);
 });
